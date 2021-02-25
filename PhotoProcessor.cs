2a2111a74957a9edaaa7ac00989f9648c91500a1
Gabriel Vega <gvega001@gmail.com>
2021-02-25 11:16:02 -0800

    Implemented photo processor class.

--- PhotoProcessor.cs
+++ PhotoProcessor.cs
@@ -2,6 +2,17 @@
 {
     public class PhotoProcessor
     {
+        public void Process(string path)
+        {
+            var photo = Photo.Load(path);
 
+            var filters = new PhotoFilters();
+
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+
+            photo.Save();
+        }
     }
 }
